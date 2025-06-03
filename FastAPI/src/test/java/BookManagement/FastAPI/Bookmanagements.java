ae374475ec8a26d0c4bfcbe30ef627232523fab0
rajivkf0@gmail.com <rajivkf0@gmail.com>
2025-06-03 18:52:26 +0530

    New changes

--- FastAPI/src/test/java/BookManagement/FastAPI/Bookmanagements.java
+++ FastAPI/src/test/java/BookManagement/FastAPI/Bookmanagements.java
@@ -23,27 +23,25 @@ public class Bookmanagements {
 		ResponseSpecification res200 = new ResponseSpecBuilder().expectStatusCode(200)
 				.expectContentType(ContentType.JSON).build();
 
-
 		// -----------------------Login for the FASTAPI - Bookmanagement----------------
 
 		LoginRequest login = new LoginRequest();
 		login.setEmail("Test3@grr.la");
 		login.setPassword("Test@1234");
 
-		LoginResponse loginres = given().spec(req).body(login)
-				.when()
-				.post(FastAPIResources.Login())
-				.then().spec(res200).extract().response()
-				.as(LoginResponse.class);
+		LoginResponse loginres = given().spec(req).body(login).when().post(FastAPIResources.Login()).then().spec(res200)
+				.extract().response().as(LoginResponse.class);
 
 		accessToken = loginres.getAccess_token();
 		System.out.println("User logged in successfully");
 
-		// -----------------------CreateBook for the FASTAPI -Bookmanagement----------------
+		// -----------------------CreateBook for the FASTAPI
+		// -Bookmanagement----------------
 
 		CreateBookRequest createBook = new CreateBookRequest();
 		createBook.setAuthor("John Will");
-		createBook.setBook_summary("Dive into the world of REST APIs and master the art of automated testing with this practical, hands-on guide.");
+		createBook.setBook_summary(
+				"Dive into the world of REST APIs and master the art of automated testing with this practical, hands-on guide.");
 		createBook.setName("Rest Assured API Testing");
 		createBook.setPrice(20);
 		createBook.setPublished_year("2024");
@@ -52,56 +50,48 @@ public class Bookmanagements {
 				.setAccept(ContentType.JSON).setContentType(ContentType.JSON)
 				.addHeader("Authorization", "Bearer " + accessToken).build();
 
-		CreateBookResponse newBook = given().spec(reqAuth).body(createBook)
-				.when()
-				.post(FastAPIResources.CreateBook())
-				.then().spec(res200).extract().response()
-				.as(CreateBookResponse.class);
+		CreateBookResponse newBook = given().spec(reqAuth).body(createBook).when().post(FastAPIResources.CreateBook())
+				.then().spec(res200).extract().response().as(CreateBookResponse.class);
 
 		String bookID = newBook.getId();
-		
-		System.out.println("The Book has been created with the id: "+ bookID);
 
+		System.out.println("The Book has been created with the id: " + bookID);
 
-		// -----------------------Get books by Book_ID for the FASTAPI -Bookmanagement----------------
+		// -----------------------Get books by Book_ID for the FASTAPI
+		// -Bookmanagement----------------
 
-		GetBookByIDResponse getbookByID = given().pathParam("book_id", bookID).spec(reqAuth)
-				.when()
-				.get(FastAPIResources.GetBookById())
-				.then().spec(res200).extract().response()
+		GetBookByIDResponse getbookByID = given().pathParam("book_id", bookID).spec(reqAuth).when()
+				.get(FastAPIResources.GetBookById()).then().spec(res200).extract().response()
 				.as(GetBookByIDResponse.class);
 
 		String getBookName = getbookByID.getName();
 
-		System.out.println("The book name searched by Book_ID "+bookID+" is:  " +getBookName);
+		System.out.println("The book name searched by Book_ID " + bookID + " is:  " + getBookName);
 
-		
-		// -----------------------UPDATE books using Book_ID for the FASTAPI -Bookmanagement----------------
+		// -----------------------UPDATE books using Book_ID for the FASTAPI
+		// -Bookmanagement----------------
 
 		UpdateBookRequest updateBook = new UpdateBookRequest();
 		updateBook.setAuthor("Rahul Shetty");
-		updateBook.setBook_summary("This comprehensive resource takes you on a journey from the foundational principles of RESTful architecture and HTTP methods to the intricacies of building robust test automation frameworks.");
+		updateBook.setBook_summary(
+				"This comprehensive resource takes you on a journey from the foundational principles of RESTful architecture and HTTP methods to the intricacies of building robust test automation frameworks.");
 		updateBook.setName("Mastering Rest API Automation with Rest Assured");
 		updateBook.setPrice(30);
 		updateBook.setPublished_year("2025");
 
-		UpdateBookResponse updatebook = given().pathParam("book_id", bookID).spec(reqAuth).body(updateBook)
-				.when()
-				.put(FastAPIResources.UpdateBookByID())
-				.then().spec(res200).extract().response()
+		UpdateBookResponse updatebook = given().pathParam("book_id", bookID).spec(reqAuth).body(updateBook).when()
+				.put(FastAPIResources.UpdateBookByID()).then().spec(res200).extract().response()
 				.as(UpdateBookResponse.class);
 
 		String updatedBookName = updatebook.getName();
 
-		System.out.println("The updated book name searched by Book_ID "+bookID+" is:  " + updatedBookName);
+		System.out.println("The updated book name searched by Book_ID " + bookID + " is:  " + updatedBookName);
 
-		
-		// -----------------------Delete books by using Book_IDfor the FASTAPI -Bookmanagement----------------
+		// -----------------------Delete books by using Book_IDfor the FASTAPI
+		// -Bookmanagement----------------
 
-		DeleteBookByIDResponse deleteBookID = given().pathParam("book_id", bookID).spec(reqAuth).body(updateBook)
-				.when()
-				.delete(FastAPIResources.DeleteBookByID())
-				.then().spec(res200).extract().response()
+		DeleteBookByIDResponse deleteBookID = given().pathParam("book_id", bookID).spec(reqAuth).body(updateBook).when()
+				.delete(FastAPIResources.DeleteBookByID()).then().spec(res200).extract().response()
 				.as(DeleteBookByIDResponse.class);
 
 		String message = deleteBookID.getMessage();
